565e44dcc9922fa36381a21836d78e70d1461df7
Shaurya Singh <singhshaurya2.7182@gmail.com>
2026-05-27 15:01:57 +0530

    Update project

--- frontend/.next/dev/server/chunks/ssr/frontend_src_06mpn9d._.js
+++ frontend/.next/dev/server/chunks/ssr/frontend_src_06mpn9d._.js
@@ -2672,7 +2672,7 @@ function Dashboard() {
                                             " This search executes raw interpolation:",
                                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$frontend$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("code", {
                                                 className: "block bg-black/10 dark:bg-black/30 p-1.5 rounded mt-1 font-mono",
-                                                children: "SELECT * FROM \"Doctor\" WHERE name ILIKE '%{query}%'                "
+                                                children: "SELECT * FROM \"Doctor\" WHERE name ILIKE '%{query}%'                 "
                                             }, void 0, false, {
                                                 fileName: "[project]/frontend/src/app/dashboard/page.js",
                                                 lineNumber: 1131,
